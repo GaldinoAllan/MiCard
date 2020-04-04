fbd1cf2b2ac04e5b3815071daa1e2829d2cf71e6
Allan <allan.g.galdino@outlook.com>
2020-04-04 13:47:09 -0300

    Initial Commit

---
+++ AllanCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AllanCard
+//
+//  Created by Allan on 04/04/20.
+//  Copyright © 2020 Allan Galdino. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
