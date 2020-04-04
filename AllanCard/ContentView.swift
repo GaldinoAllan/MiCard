046d2c4fdae2c1cc0ec2fdffd1f48cc2a864a5b2
Allan <allan.g.galdino@outlook.com>
2020-04-04 16:28:11 -0300

    Responsive layouts with SwiftUI

--- AllanCard/ContentView.swift
+++ AllanCard/ContentView.swift
@@ -10,7 +10,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Color(red:0.91, green:0.30, blue:0.24)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("fotoMinha")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200)
+                    .clipShape(Circle())
+                    .overlay(
+                        Circle().stroke(Color.white, lineWidth: 5)
+                )
+                Text("Allan Galdino")
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                Text("iOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                Divider()
+                InfoView(text: "+55 (19) 99850-1170", imageName: "phone.fill")
+                InfoView(text: "gagaldinoallan@gmail.com", imageName: "envelope.fill")
+            }
+        }
     }
 }
 
@@ -19,3 +42,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
---
+++ AllanCard/InfoView.swift
@@ -0,0 +1,34 @@
+//
+//  InfoView.swift
+//  AllanCard
+//
+//  Created by Allan on 04/04/20.
+//  Copyright © 2020 Allan Galdino. All rights reserved.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    
+    let text: String
+    let imageName: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 50)
+            .fill(Color.white)
+            .frame(height: 50)
+            .overlay(HStack {
+                Image(systemName: imageName)
+                    .foregroundColor(.green)
+                Text(text)
+            })
+            .padding(.all)
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView(text: "Hello", imageName: "phone.fill")
+            .previewLayout(.sizeThatFits)
+    }
+}
